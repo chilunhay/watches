ae9514f2cce21aa6210e4e54a8cac2664dd607c5
chilunhay <72005042+chilunhay@users.noreply.github.com>
2022-11-07 10:30:15 +0700

    fix 1

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,6 @@ const product = require("./routes/ProductRoute");
 const user = require("./routes/UserRoute");
 const order = require("./routes/OrderRoute");
 const payment = require("./routes/PaymentRoute");
-const cart = require("./routes/WishListRoute");
 
 app.use("/api/v2", product);
 
@@ -33,8 +32,6 @@ app.use("/api/v2", user);
 app.use("/api/v2", order);
 
 app.use("/api/v2", payment);
-
-app.use("/api/v2", cart);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
