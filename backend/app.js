1cd6f3f5068ff91546a1badd02a125e41b9388e7
chilunhay <72005042+chilunhay@users.noreply.github.com>
2023-02-23 17:53:31 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -35,12 +35,6 @@ app.use("/api/v2", order);
 
 app.use("/api/v2", payment);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
-
 // it's for errorHandeling
 app.use(ErrorHandler);
 
