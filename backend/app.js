0cc6015828e9f5c1ef0440f906c710692134b13c
chilunhay <72005042+chilunhay@users.noreply.github.com>
2023-02-23 17:27:11 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,10 @@ const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 const bodyParser = require("body-parser");
 const path = require("path");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
